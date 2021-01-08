47c58d7dceec3c55a7ec4ec97da9c146bdb59b11
Esteban Salazar <70934246+este2897@users.noreply.github.com>
2021-01-08 16:28:07 -0600

    Add files via upload

--- MUXsrcALU.v
+++ MUXsrcALU.v
@@ -1,15 +1,15 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
+// Company: MT-7003
+// Engineer: E. SALAZAR
 // 
 // Create Date:    21:09:50 01/08/2021 
 // Design Name: 
 // Module Name:    MUXsrcALU 
-// Project Name: 
+// Project Name: 		RISC-V-ISA
 // Target Devices: 
 // Tool versions: 
-// Description: 
+// Description: mux module for ALU source
 //
 // Dependencies: 
 //
@@ -18,12 +18,14 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
+
 module MUXsrcALU(
-    input [31:0] RD2,
-    input [31:0] ImmGEN,
-	 input ALUsrc,
-    output reg [31:0] ALU2);
-	
+	// MUX 2:1 FOR ALU SOURCE
+    input [31:0] RD2, // REG 2 FROM REG FILE 
+    input [31:0] ImmGEN, // IMMEDIATE 
+	 input ALUsrc, //FROM CU
+    output reg [31:0] ALU2); //OUTPUT FOR ALU SOURCE 2
+	// CASE FOR MUX, 1->IMM, 0->RD2
    always @ (RD2 or ImmGEN or ALUsrc) begin  
       case (ALUsrc)  
          1'b0 : ALU2 <= RD2;  
