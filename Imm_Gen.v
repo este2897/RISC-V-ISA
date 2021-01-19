db1a9f5a922ca33e4336d307379c8e602d2395f3
esmontero <70980649+esmontero@users.noreply.github.com>
2021-01-18 23:38:33 -0600

    Update Imm_Gen.v

--- Imm_Gen.v
+++ Imm_Gen.v
@@ -27,11 +27,11 @@ module Imm_Gen(
 
 always @(*) begin
   case (ImmSel)
-    2'b000 : ExtImm={20{Instr[31]}, Instr[31:20]}; //Instruccion tipo I
-    2'b001 : ExtImm={20{Instr[31]}, Instr[30:25], Instr[11:7]}; // Instruccion tipo S
-    2'b010 : ExtImm={19{Instr[31]}, Instr[7], Instr[30:25], Instr[11:8], 1'b0}; //Instruccion tipo B
-    2'b011 : ExtImm={Instr[31:12], 12'b0}; //Instruccion tipo U
-    2'b100 : ExtImm={11{Instr[31]}, Instr[19:12], Instr[20], Instr[30:21], 1'b0}; //Instruccion tipo J
+    3'b000 : ExtImm={20{Instr[31]}, Instr[31:20]}; //Instruccion tipo I
+    3'b001 : ExtImm={20{Instr[31]}, Instr[30:25], Instr[11:7]}; // Instruccion tipo S
+    3'b010 : ExtImm={19{Instr[31]}, Instr[7], Instr[30:25], Instr[11:8], 1'b0}; //Instruccion tipo B
+    3'b011 : ExtImm={Instr[31:12], 12'b0}; //Instruccion tipo U
+    3'b100 : ExtImm={11{Instr[31]}, Instr[19:12], Instr[20], Instr[30:21], 1'b0}; //Instruccion tipo J
 
   end
 
