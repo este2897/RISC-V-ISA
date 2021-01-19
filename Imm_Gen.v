c74a3bb7a006eadc3a0de602e4e70dc7d91d538a
Esteban Salazar <70934246+este2897@users.noreply.github.com>
2021-01-18 23:44:07 -0600

    Update Imm_Gen.v

--- Imm_Gen.v
+++ Imm_Gen.v
@@ -22,17 +22,17 @@
 module Imm_Gen(
   input [31:0] Instr,
   input [2:0] ImmSel, //FROM CU
-  output [31:0] ExtImm);
+  output reg [31:0] ExtImm);
 
 
 always @(*) begin
   case (ImmSel)
-    3'b000 : ExtImm={20{Instr[31]}, Instr[31:20]}; //Instruccion tipo I
-    3'b001 : ExtImm={20{Instr[31]}, Instr[30:25], Instr[11:7]}; // Instruccion tipo S
-    3'b010 : ExtImm={19{Instr[31]}, Instr[7], Instr[30:25], Instr[11:8], 1'b0}; //Instruccion tipo B
+//  3'b000 : ExtImm={20{Instr[31]}, Instr[31:20]}; //Instruccion tipo I
+//  3'b001 : ExtImm={20{Instr[31]}, Instr[30:25], Instr[11:7]}; // Instruccion tipo S
+//  3'b010 : ExtImm={19{Instr[31]}, Instr[7], Instr[30:25], Instr[11:8], 1'b0}; //Instruccion tipo B
     3'b011 : ExtImm={Instr[31:12], 12'b0}; //Instruccion tipo U
-    3'b100 : ExtImm={11{Instr[31]}, Instr[19:12], Instr[20], Instr[30:21], 1'b0}; //Instruccion tipo J
-
-  end
+//  3'b100 : ExtImm={11{Instr[31]}, Instr[19:12], Instr[20], Instr[30:21], 1'b0}; //Instruccion tipo J
+  endcase
+end
 
 endmodule
