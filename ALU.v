fbe863bec58247b635955779f0b7d66052f9c738
esmontero <70980649+esmontero@users.noreply.github.com>
2021-01-19 21:58:06 -0600

    Update ALU.v
    Agregada la operación de shift aritmetico

--- ALU.v
+++ ALU.v
@@ -33,7 +33,7 @@ begin
  case(alu_control)
  3'b000: result = a + b; // suma
  3'b001: result = a - b; // resta
- 3'b010: result = ~a;
+ 3'b010: result = a>>>b; //shift aritmetico
  3'b011: result = a<<b;
  3'b100: result = a>>b;
  3'b101: result = a & b; // and
