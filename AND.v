0e5b8ce3ee965735be867ec909bc9b731a8314cc
Esteban Salazar <70934246+este2897@users.noreply.github.com>
2021-01-21 01:23:47 -0600

    Compuerta AND

---
+++ AND.v
@@ -0,0 +1,23 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    07:55:17 01/20/2021 
+// Design Name: 
+// Module Name:    AND 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module AND(input a, b, output y);
+	and(y, a, b);
+endmodule
