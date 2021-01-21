cae0a490f361e885afab9e9869cecd5fe2581e76
Esteban Salazar <70934246+este2897@users.noreply.github.com>
2021-01-21 00:53:09 -0600

    Módulo PC siguiente
    almacena el pc siguiente y se lo asigna a la salida para que lo reciba la IM

---
+++ PC_next.v
@@ -0,0 +1,35 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    05:17:30 01/21/2021 
+// Design Name: 
+// Module Name:    PC_next 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module PC_next(
+    input CLK,
+	 input reset,
+    input [31:0] pc_next,
+    output reg [31:0] pc_actual
+    );
+	 
+	 initial pc_actual = 0;
+	 
+	 always @(posedge CLK)	 
+	 	if (!reset)
+			pc_actual = pc_next;
+		else
+			pc_actual = 0;
+endmodule
