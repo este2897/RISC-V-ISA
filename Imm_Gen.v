e154f7de82c57b2996d3e35d643030c63c0f7e49
esmontero <70980649+esmontero@users.noreply.github.com>
2021-01-21 17:40:35 -0600

    Imm_Gen.v
    if para la excepión de realizar srai en las intrucciones tipo I

--- Imm_Gen.v
+++ Imm_Gen.v
@@ -22,12 +22,15 @@
 module Imm_Gen(
   input [31:0] Instr,
   input [2:0] ImmSel, //FROM CU
-  output reg [31:0] ExtImm);
+  output reg [31:0] ExtImm); //Inmediato extendido y acomodado a 32 bits
 
 
 always @(*) begin
   case (ImmSel)
-    3'b000 : ExtImm={ {21{Instr[31]}}, Instr[30:20] }; //Instruccion tipo I
+    3'b000 : begin
+		if (Instr[31:26] == 6'b010000) ExtImm={ {30{Instr[31]}}, Instr[25:20] }; //srai, el imm sin el 1 del bit 30
+		else ExtImm={ {21{Instr[31]}}, Instr[30:20] }; //Instruccion tipo I
+	 end
     3'b001 : ExtImm={ {21{Instr[31]}}, Instr[30:25], Instr[11:7] }; // Instruccion tipo S
     3'b010 : ExtImm={ {20{Instr[31]}}, Instr[7], Instr[30:25], Instr[11:8], 1'b0 }; //Instruccion tipo B
     3'b011 : ExtImm={ Instr[31:12], 12'b0 }; //Instruccion tipo U
