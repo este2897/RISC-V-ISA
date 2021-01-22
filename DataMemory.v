c335f1b442cc66f7f6cc5954fc1252b7ee1475c4
esmontero <70980649+esmontero@users.noreply.github.com>
2021-01-22 15:19:45 -0600

    Update DataMemory.v
    Cambios para implementar sb y lb

--- DataMemory.v
+++ DataMemory.v
@@ -25,6 +25,8 @@ module DataMemory(
     input [31:0] WriteData,
     input MemW,
 	 input CLK,
+	 input byte_cnt,
+
 //SALIDA
     output reg [31:0] ReadData
     );
@@ -34,11 +36,49 @@ module DataMemory(
 	 
 	 reg [SIZE-1:0] ram [MEM_DEPTH-1:0]; //INICIALIZACION DE MEMORIA
 	 
-	 initial	$readmemb("/home/ise/PROYECTO/Verilog/DataMem.mem", ram); //VALORES INICIALES
+	 initial	$readmemb("D:/TEC/Microcontroladores/Proyecto/memfile.mem", ram); //VALORES INICIALES
 
-		always @(posedge CLK)
-			begin 
-				if (MemW) ram[Address]<= WriteData; //ESCRIBIR
-			ReadData = ram[Address]; //LEER	
+		always @(posedge CLK) 
+		begin
+		if (byte_cnt) begin
+			case (Address[1:0])
+				1'b00: begin
+					if (MemW) ram[Address]<= WriteData[7:0]; //ESCRIBIR SOLO PRIMER BYTE
+				end
+				1'b01: begin
+					if (MemW) ram[Address][15:8]<= WriteData[15:8]; //ESCRIBIR SOLO SEGUNDO BYTE
+				end
+				1'b10: begin
+					if (MemW) ram[Address][23:16]<= WriteData[23:16]; //ESCRIBIR SOLO TERCER BYTE
+				end
+				1'b11: begin
+					if (MemW) ram[Address][31:24]<= WriteData[31:24]; //ESCRIBIR SOLO ULTIMO BYTE
+				end
+			endcase
+		end else begin
+			if (MemW) ram[Address]<= WriteData; //ESCRIBIR
 			end
+		end
+		
+		always @(Address) 
+		begin
+		if (byte_cnt) begin
+			case (Address[1:0])
+				1'b00: begin
+					ReadData = { 24'b0, {ram[Address][7:0]}}; //LEER SOLO PRIMER BYTE
+				end
+				1'b01: begin
+					ReadData = { 16'b0, {ram[Address][15:8]}, 8'b0}; //LEER SOLO SEGUNDO BYTE
+				end
+				1'b10: begin
+					ReadData = { 8'b0, {ram[Address][23:16]}, 16'b0}; //LEER SOLO TERCER BYTE
+				end
+				1'b11: begin
+					ReadData = {{ram[Address][31:24]}, 24'b0}; //LEER SOLO ULTIMO BYTE
+				end
+			endcase
+		end else begin
+			ReadData = ram[Address]; //LEER
+		end
+		end
 endmodule
